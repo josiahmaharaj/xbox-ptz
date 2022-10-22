2456f4b6f5efe8e435c01f8ec0705aadef596bec
Josiah Maharaj <josiahmaharaj@outlook.com>
2022-10-22 01:58:52 -0400

    working so far

--- xbox.py
+++ xbox.py
@@ -1,4 +1,8 @@
+from ast import match_case
+from asyncio import TimerHandle
 import json
+from time import sleep
+from turtle import home
 from onvif import ONVIFCamera
 import sys, os
 sys.stdout = open(os.devnull, 'w')
@@ -11,6 +15,7 @@ dead_zone = 0.09
 last_button_pressed = "None"
 flip = False
 slow = False
+
 axis_left_pan = 0.000
 axis_left_tilt = 0.000
 axis_right_pan = 0.000
@@ -22,12 +27,19 @@ cam2 = None
 cam3 = None
 cam4 = None
 
-is_running = True
+cam1Active = False
+cam2Active = False
+
+is_running = False
 controller = None
 
-stop_called = True
+#debouncers
+stop_called = False
 home_called = False
-basedir = os.path.dirname(sys.executable) if not DEV else './'
+flip_called = False
+slow_called = False
+
+basedir = './' if DEV else  os.path.dirname(sys.executable) 
 
 class PtzControl(object):
     def __init__(self, ip,  port, user, password):
@@ -44,6 +56,10 @@ class PtzControl(object):
         # Get available PTZ services
         request = self.ptz.create_type('GetServiceCapabilities')
         Service_Capabilities = self.ptz.GetServiceCapabilities(request)
+
+        # IMG controls  -------------------------------------------------------------
+        self.img = self.mycam.create_imaging_service()
+        print(self.img.Capabilities())
 
         # Get PTZ status
         status = self.ptz.GetStatus({'ProfileToken': token})
@@ -65,30 +81,6 @@ class PtzControl(object):
             self.requestc.Velocity.Zoom.space = ptz_configuration_options.Spaces.ContinuousZoomVelocitySpace[
                 0].URI
 
-        # get absoluteMove request -- requesta
-        self.requesta = self.ptz.create_type('AbsoluteMove')
-        self.requesta.ProfileToken = self.media_profile.token
-        if self.requesta.Position is None:
-            self.requesta.Position = self.ptz.GetStatus(
-                {'ProfileToken': self.media_profile.token}).Position
-        if self.requesta.Speed is None:
-            self.requesta.Speed = self.ptz.GetStatus(
-                {'ProfileToken': self.media_profile.token}).Position
-
-        # get relativeMove request -- requestr
-        self.requestr = self.ptz.create_type('RelativeMove')
-        self.requestr.ProfileToken = self.media_profile.token
-        if self.requestr.Translation is None:
-            self.requestr.Translation = self.ptz.GetStatus(
-                {'ProfileToken': self.media_profile.token}).Position
-            self.requestr.Translation.PanTilt.space = ptz_configuration_options.Spaces.RelativePanTiltTranslationSpace[
-                0].URI
-            self.requestr.Translation.Zoom.space = ptz_configuration_options.Spaces.RelativeZoomTranslationSpace[
-                0].URI
-        if self.requestr.Speed is None:
-            self.requestr.Speed = self.ptz.GetStatus(
-                {'ProfileToken': self.media_profile.token}).Position
-
         self.requests = self.ptz.create_type('Stop')
         self.requests.ProfileToken = self.media_profile.token
         self.requestp = self.ptz.create_type('SetPreset')
@@ -97,6 +89,10 @@ class PtzControl(object):
         self.requestg.ProfileToken = self.media_profile.token
         self.requestHome = self.ptz.create_type('GotoHomePosition')
         self.requestHome.ProfileToken = self.media_profile.token
+
+        # self.requestf = self.img.create_type('RelativeFocus')
+        # self.requestf.ProfileToken = self.media_profile.token
+
         self.stop()
 
     # Stop pan, tilt and zoom
@@ -138,11 +134,13 @@ pygame.init()
 
 def create_camera(camera):
     cam = None
+    print('Loading', camera['name'])
+    print('...')
     try:
         cam = PtzControl(camera['ip'], camera['port'], camera['user'], camera['password'])
         return cam
     except:
-        print("could not load ", camera['name'])
+        print("could not load", camera['name'])
         return cam
 
 def load_cameras():
@@ -150,33 +148,37 @@ def load_cameras():
     path = os.path.join(basedir, 'cameras.json')
     if os.path.exists(path):
         print('Loading cameras. Please wait for complete message...')
+        print()
         f = open(path)
         cameras = json.load(f)
         cam1 = create_camera(cameras['cam1'])
-        cam2 = create_camera(cameras['cam2'])
+        # cam2 = create_camera(cameras['cam2'])
         print('Camera load completed!')
+        print("")
+        print("")
     else:
         print('cameras.json file missing. Create a cameras.json file in this directory and restart')
 
-def set_active_cam(dpad, controller):
-    global active_cam, cam1, cam2, cam3, cam4
-    if(dpad[1] == 1):
-        print("cam1 active")
-        active_cam = cam1
-        controller.rumble(0,0.5,500)
-    if(dpad[0] == 1):
-        print("cam2 active")
-        active_cam = cam2
-        controller.rumble(0,0.5,500)
-    if(dpad[1] == -1):
-        print("cam3 active")
-        active_cam = cam3
-        controller.rumble(0,0.5,500)
-    if(dpad[0] == -1):
-        print("cam4 active")
-        active_cam = cam4
-        controller.rumble(0,0.5,500)
+def set_active_cam( controller):
+    global active_cam, cam1, cam2, cam1Active, cam2Active
+    if controller.get_button(4):
+        if not cam1Active:
+            print("cam1 active")
+            active_cam = cam1
+            cam1Active = True
+            cam2Active = False
+            controller.rumble(0,0.5,500)
 
+    if controller.get_button(5):
+        if not cam2Active:
+            print("cam2 active")
+            active_cam = cam2
+            cam1Active = False
+            cam2Active = True
+            controller.rumble(0,0.5,500)
+
+
+### CALLS
 def call_continuous_movement():
     global stop_called, axis_left_tilt, axis_left_pan,axis_right_tilt, flip
     stop_called = False
@@ -186,25 +188,37 @@ def call_continuous_movement():
 
     pan = pan/2 if slow else pan
     tilt = tilt/2 if slow else tilt
-    zoom = zoom/2 if slow else zoom
+    zoom = zoom/4 if slow else zoom
     active_cam.move_continuous(pan, tilt, zoom)
 
 def call_home():
-    active_cam.call_home()
+    global home_called
+    if  home_called:
+        active_cam.call_home()
+        home_called = False
 
 def call_stop():
     global stop_called
-    if not stop_called:
+    if  stop_called:
         active_cam.stop()
-        print('Stopped')
-        stop_called = True
+        stop_called = False
     
+def call_flip():
+    global flip
+    flip = not flip
+        
+def call_slow():
+    global slow
+    slow = not slow
+
+### HANDLERS
 def controller_handler(controller):
-    global last_button_pressed, axis_left_pan, axis_left_tilt, axis_right_pan, axis_right_tilt, cam
+    global last_button_pressed, axis_left_pan, axis_left_tilt, axis_right_pan, axis_right_tilt, stop_called
 
     axis_left_x = 0.000
     axis_left_y = 0.000
     axis_right_y = 0.000
+
     if(controller.get_axis(0) > dead_zone or controller.get_axis(0) < -dead_zone):
         axis_left_x = round(controller.get_axis(0),3)
     if(controller.get_axis(1) > dead_zone or controller.get_axis(1) < -dead_zone):
@@ -218,49 +232,47 @@ def controller_handler(controller):
         axis_right_tilt = axis_right_y
         # if( axis_left_x <= dead_zone and axis_left_x >= -dead_zone and axis_left_y <= dead_zone and axis_left_y >= -dead_zone and axis_right_y <= dead_zone and axis_right_y >= -dead_zone ):
         if(axis_left_pan == 0.0 and axis_right_pan == 0.0 and axis_right_tilt == 0.0):
+            stop_called = True
             call_stop()
         # print(str(axis_left_pan) + ', ' + str(axis_left_tilt) + ', ' + str(axis_right_tilt))
         call_continuous_movement()
 
-    # axis_right_x = round(controller.get_axis(3),3)
-    if controller.get_button(0):
-        last_button_pressed = "A"
-        print("BUTTON A")
+def button_selection(button):
+    global is_running
+    match button:
+        case 0: #A
+            call_flip()
+        case 1: #B
+            call_slow()
+        case 2: #X
+            pass
+        case 3: #Y
+            pass
+        case 4: #LB
+            pass
+        case 5: #RB
+            pass
+        case 6: #Back
+            is_running = False
+        case 7: #Start
+            is_running = True
+        case 8: #LS
+            call_home()
+        case 9: #RS
+            pass
+        case _:
+            pass
 
-    if controller.get_button(1):
-        last_button_pressed = "B"
-        print("BUTTON B")
-
-    if controller.get_button(2):
-        last_button_pressed = "X"
-        print("BUTTON X")
-
-    if controller.get_button(3):
-        last_button_pressed = "Y"
-        print("BUTTON Y")
-
-    if controller.get_button(4):
-        last_button_pressed = "LB"
-        print("BUTTON LB")
-
-    if controller.get_button(5):
-        last_button_pressed = "RB"
-        print("BUTTON RB")
-
-    if controller.get_button(6):
-        print("BUTTON OPT")
-        is_running = False
-        controller.quit()
-    
-    if controller.get_button(7):
-        last_button_pressed = "MENU"
-        print("BUTTON MENU")
-
-    if controller.get_button(8):
-        print("LT Pressed")
-        call_home()
-    if controller.get_button(10):
-        print("D Pad")
+def dpad_actions(dpad, controller):
+    print(dpad)
+    if(dpad[1] == 1):
+        pass
+    if(dpad[0] == 1):
+        pass
+    if(dpad[1] == -1):
+        pass
+    if(dpad[0] == -1):
+        pass
 
 def check_for_controller():
     print("checking for controller")
@@ -276,24 +288,44 @@ def check_for_controller():
         print("no joystick connected")
         None
 
-controller = check_for_controller()
-if controller != None:
-    power = controller.get_power_level
-    controller.rumble(0.5,1,0)
+def initialize():
+    global controller, is_running
+    print(f"TWEC PTZ Controller")
+    print(f"Author: Josiah Maharaj")
+    print(f"Last Updated: 20221022")
+    print("############################################")
+    print()
+    print()
+    sleep(1)
+
+    load_cameras()
+    controller = check_for_controller()
+    if controller != None:
+        is_running = True
+        controller.rumble(0.5,1,0)
+        print("Program active! Use LB or RB to select camera.")
+        print("----------------------------------------------")
+        print()
+    else:
+        print("No Controllers found. Quit and Reopen")
 
 
-load_cameras()
 try:
+    initialize()
+
     while is_running == True:
         for event in pygame.event.get():
             if event.type == pygame.JOYHATMOTION:
-                set_active_cam(event.value, controller)
+                dpad_actions(event.value, controller)
             if event.type == pygame.QUIT:
                 is_running = False
+            if event.type == pygame.JOYBUTTONUP:
+                button_selection(event.button)
+        
+        if controller != None:
+            set_active_cam(controller)
 
         if(controller != None and active_cam != None):
             controller_handler(controller)
-        # if(controller == None):
-        #     controller = check_for_controller()
 except:
     print("Error")
